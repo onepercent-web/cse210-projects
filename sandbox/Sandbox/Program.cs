b2a2c4f4db3a617bdeaf5e8fab8ea98f6ea2d02d
onepercent-web <keyami2019@gmail.com>
2024-04-28 18:39:48 +0900

    Finished prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
+
     }
 }
