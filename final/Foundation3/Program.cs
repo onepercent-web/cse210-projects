a5af1687bdf3c38ebf113a3a7a707b05624f3ab2
onepercent-web <keyami2019@gmail.com>
2024-06-06 14:07:28 +0900

    Finished final3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-// Addressクラスは住所の情報を保持します
+// Address: class
 class Address
 {
     private string street;
@@ -8,6 +8,7 @@ class Address
     private string state;
     private string country;
 
+　　// Constructor: Set each address information
     public Address(string street, string city, string state, string country)
     {
         this.street = street;
@@ -16,13 +17,14 @@ class Address
         this.country = country;
     }
 
+　　// Obtain address information as a string
     public override string ToString()
     {
-        return $"{street}\n{city}, {state}\n{country}";
+        return $"{street}\n{city}, {state}, {country}";
     }
 }
 
-// Eventクラスはイベントの共通情報を保持します
+// Event: class
 class Event
 {
     protected string title;
@@ -31,6 +33,7 @@ class Event
     protected DateTime time;
     protected Address address;
 
+    // Constructor: Sets common information for events
     public Event(string title, string description, DateTime date, DateTime time, Address address)
     {
         this.title = title;
@@ -40,94 +43,102 @@ class Event
         this.address = address;
     }
 
+    // Get standard event details
     public string GetStandardDetails()
     {
         return $"{title}\n{date.ToShortDateString()} @ {time.ToShortTimeString()}\n{address}";
     }
 
+    // Get full event details
     public virtual string GetFullDetails()
     {
         return $"{GetStandardDetails()}";
     }
 
+    // Get short event description
     public string GetShortDescription()
     {
-        return $"{this.GetType().Name} - {title} - {date.ToShortDateString()}";
+        return $"{this.GetType().Name} - {title.Split(" - ")[0]} - {date.ToShortDateString()}";
     }
 }
 
-// Lectureクラスは講演会の情報を保持します
+// Lecture: class
 class Lecture : Event
 {
     private string speaker;
     private int capacity;
 
+    // Constructor: Set lecture information
     public Lecture(string title, string description, DateTime date, DateTime time, Address address, string speaker, int capacity)
         : base(title, description, date, time, address)
     {
         this.speaker = speaker;
         this.capacity = capacity;
     }
-
+    // Get full lecture details
     public override string GetFullDetails()
     {
         return $"{GetStandardDetails()}\nSpeaker: {speaker}\nCapacity: {capacity}";
     }
 }
 
-// Receptionクラスはレセプションの情報を保持します
+// Reception: class
 class Reception : Event
 {
     private string rsvpEmail;
 
+    // Constructor: Set reception information
     public Reception(string title, string description, DateTime date, DateTime time, Address address, string rsvpEmail)
         : base(title, description, date, time, address)
     {
         this.rsvpEmail = rsvpEmail;
     }
 
+    // Get full reception details
     public override string GetFullDetails()
     {
         return $"{GetStandardDetails()}\nEmail: {rsvpEmail}";
     }
 }
 
-// OutdoorGatheringクラスは屋外集会の情報を保持します
-class OutdoorGathering : Event
+// outdoor: class
+class Outdoor : Event
 {
     private string weather;
-
-    public OutdoorGathering(string title, string description, DateTime date, DateTime time, Address address, string weather)
+    
+    // Constructor: Set outdoor information
+    public Outdoor(string title, string description, DateTime date, DateTime time, Address address, string weather)
         : base(title, description, date, time, address)
     {
         this.weather = weather;
     }
 
+    // Get full outdoor details
     public override string GetFullDetails()
     {
         return $"{GetStandardDetails()}\nWeather: {weather}";
     }
 }
 
-// メインプログラム
+// Program: class
 class Program
 {
     static void Main(string[] args)
     {
-        // 住所オブジェクトを作成
+        // Address list
         Address address1 = new Address("108 Blue St", "Sacramento", "CA", "USA");
         Address address2 = new Address("456 Silver St", "Frankfort", "KY", "USA");
-        Address address3 = new Address("246 Oak Circle", "London", "England", "UK");
+        Address address3 = new Address("153 Light St", "Shibuya Ward", "TOKYO", "JAPAN");
 
-        // 各イベントオブジェクトを作成
-        Event lecture = new Lecture("Object Oriented Programing - Inheritance", "Learn about inheritance in OOP", new DateTime(2023, 1, 1), new DateTime(2023, 1, 1, 9, 0, 0), address1, "Bob the Builder", 100);
-        Event reception = new Reception("Graduation - MSD 321 Graduation Party", "Celebrate the graduation of MSD 321", new DateTime(2023, 6, 1), new DateTime(2023, 6, 1, 19, 0, 0), address2, "grad@msd321.com");
-        Event outdoorGathering = new OutdoorGathering("Bridge Tour - Tour the London Bridge", "Tour the historic London Bridge", new DateTime(2023, 10, 13), new DateTime(2023, 10, 13, 11, 30, 0), address3, "Sunny");
+        // Event list
+        Event lecture = new Lecture("Object Oriented Programing - Inheritance", "Learn about inheritance in OOP", new DateTime(2024, 10, 2), new DateTime(2024, 10, 2, 18, 0, 0), address1, "Logan White", 500);
+        Event reception = new Reception("Graduation - SHS 777 Graduation Party", "Celebrate the graduation of SHS 777", new DateTime(2025, 5, 16), new DateTime(2025, 5, 16, 10, 0, 0), address2, "newstars@shs777.com");
+        Event outdoor = new Outdoor("Japan Tour - Tour the Japanese Rice Ball Shops", "Tour the historic Japanese Rice Ball Shops", new DateTime(2024, 7, 1), new DateTime(2024, 7, 1, 9, 30, 0), address3, "Sunny");
 
-        // イベントオブジェクトの配列を作成
-        Event[] events = { lecture, reception, outdoorGathering };
+        // Create an array of events
+        Event[] events = { lecture, reception, outdoor };
 
-        // 各イベントの詳細を表示
+        // Display details of each event
         foreach (var eventItem in events)
         {
             Console.WriteLine(eventItem.GetStandardDetails());
